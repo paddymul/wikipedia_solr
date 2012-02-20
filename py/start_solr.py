8727c069878b24944e75889310e80d84c1d3eac4
paddymul <paddy@paddymullen.com>
2012-02-20 17:47:38 -0500

    now it seems to work with relative paths

--- py/start_solr.py
+++ py/start_solr.py
@@ -1,5 +1,5 @@
 import os
 
-os.system("cd ../lib/solr/solr/example/; java -Dsolr.solr.home=../../../../solr_home/solr -jar start.jar")
+os.system("cd ../lib/solr/solr/example/; java -Dsolr.solr.home=../../../../solr_home/solr -Dsolr.absolutePath=`pwd`/../../../../solr_home/solr  -jar start.jar")
 
 
