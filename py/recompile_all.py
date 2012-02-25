0d911188ecebfcc79a22f05d139eaa9305e36b73
Paddy Mullen <paddy@chartwidget.com>
2012-02-25 10:36:53 -0500

    no longer needed, replaced by apps

--- py/recompile_all.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.system("cd ../lib/solr/solr/; ant clean compile example");
-os.system("cd ../solr_home/Wikipedia_importer/wikipedia_solr ; ant clean compile jar");
-
-
-
--- py/start_solr.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.system("cd ../lib/solr/solr/example/; java   -Xms512M -Xmx1024M  -Dsolr.solr.home=../../../../solr_home/solr -Dsolr.absolutePath=`pwd`/../../../../solr_home/solr  -jar start.jar")
-
-
