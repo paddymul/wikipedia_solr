e355da7aab8f2482e54c4fc856ab82caae841a7a
Paddy Mullen <paddy@chartwidget.com>
2012-02-25 10:37:07 -0500

    better error handling

--- solr_home/Wikipedia_importer/wikipedia_solr/src/wikipedia_solr/WikimediaToTextTransformer.java
+++ solr_home/Wikipedia_importer/wikipedia_solr/src/wikipedia_solr/WikimediaToTextTransformer.java
@@ -3,28 +3,33 @@
  * and open the template in the editor.
  */
 package wikipedia_solr;
-    
+
 import org.apache.solr.handler.dataimport.Transformer;
 import java.util.List;
 import java.util.Map;
 import org.apache.solr.handler.dataimport.Context;
 //import wikipedia_solr.SimpleParser;
+
 /**
  *
  * @author patrickmullen
  */
-public class WikimediaToTextTransformer extends Transformer
-{
+public class WikimediaToTextTransformer extends Transformer {
 
     public Map<String, Object> transformRow(Map<String, Object> row, Context context) {
+        try {
+            String wikiMediaText = (String) row.get("wikimediaMarkup");
+            if (wikiMediaText != null) {
+                SimpleParser sp = new SimpleParser(wikiMediaText);
+                sp.getSections();
+                row.put("articlePlainText", sp.getParagraphText());
+                row.put("sectionParsed", sp.jsonSections());
+            }
+        } catch (java.lang.Exception e) {
+            System.out.println("error with  " + row.get("title"));
+            System.out.println(e);
 
-        String wikiMediaText = (String) row.get("wikimediaMarkup");
-        if(wikiMediaText != null){
-            SimpleParser sp = new SimpleParser(wikiMediaText);
-            sp.getSections();
-            row.put("articlePlainText", sp.getParagraphText());
-            row.put("sectionParsed",    sp.jsonSections());
-                    }   
+        }
         return row;
     }
 }
